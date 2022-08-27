32bf44be2546753992892944e74f6db4c63ddd1d
allyson7 <allysonlopes0@gmail.com>
2022-08-27 12:00:39 -0400

    :construction: Creating the modal for new transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
-import logoImg from '../../assets/logo.svg';
-import { Container, Content } from './styles';
+import { useState } from "react";
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModa() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModa() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
